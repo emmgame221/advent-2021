40c29f6108aea6cea2e695f39d5270fb5eda73af
Eric McHugh <emmgame221@gmail.com>
2021-12-09 20:58:22 -0500

    Even more day10 prep

--- src/problems/day10.rs
+++ src/problems/day10.rs
@@ -1,3 +1,6 @@
+use std::{fs::File, io::{BufReader, BufRead}};
+
 pub fn print_solution() {
-    
+    let input = File::open("./inputs/input10.txt").unwrap();
+    let lines = BufReader::new(input).lines();
 }
