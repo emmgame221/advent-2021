2aaaf053f294de5809fe230dc55525f38c13e08a
Eric McHugh <emmgame221@gmail.com>
2021-12-14 01:45:35 -0500

    day 14 part 2 solved with hint also cleaned up part 1 solution

--- src/problems/day14.rs
+++ src/problems/day14.rs
@@ -4,16 +4,19 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use itertools::Itertools;
+
 pub fn print_solution() {
     let input = File::open("./inputs/input14.txt").unwrap();
     let mut lines = BufReader::new(input).lines();
-    let mut template: Vec<u8> = lines
+    let template: Vec<u8> = lines
         .next()
         .unwrap()
         .unwrap()
         .chars()
         .map(|c| c as u8)
         .collect();
+    let last = *template.last().unwrap();
     lines.next().unwrap().unwrap();
     let mut insertion_rules: HashMap<(u8, u8), u8> = HashMap::new();
     for line in lines {
@@ -26,56 +29,34 @@ pub fn print_solution() {
         let c = parts[1].chars().nth(0).unwrap() as u8;
         insertion_rules.insert(pair, c);
     }
-    for _n in 0..10 {
-        //println!("{}", n);
-        template = do_insertions(&template, &insertion_rules);
+    let mut pair_counts: HashMap<(u8, u8), usize> = HashMap::new();
+    for (a, b) in template.iter().tuple_windows() {
+        *pair_counts.entry((*a, *b)).or_default() += 1;
     }
-    let mut counts: HashMap<u8, usize> = HashMap::new();
-    for c in template.iter() {
-        let count = if let Some(n) = counts.get(c) {
-            *n + 1
-        } else {
-            1
-        };
-        counts.insert(*c, count);
-    }
-    let p1 = counts.values().max().unwrap() - counts.values().min().unwrap();
-    println!("Day 14 Part 1: {}", p1);
-    let backup = template.clone();
-    let mut counts: HashMap<u8, usize> = HashMap::new();
-    for slice in 1..(template.len() - 1) {
-        println!("Slice: {}", slice);
-        template = backup[slice - 1..=slice].to_vec();
-        for _n in 0..30 {
-            //println!("{}", n + 10);
-            template = do_insertions(&template, &insertion_rules);
+    for i in 0..40 {
+        let mut new_pair_counts = HashMap::new();
+        for (pair, count) in pair_counts.iter() {
+            let c = insertion_rules[pair];
+            let pair1 = (pair.0, c);
+            *new_pair_counts.entry(pair1).or_default() += *count;
+            let pair2 = (c, pair.1);
+            *new_pair_counts.entry(pair2).or_default() += *count;
         }
-        for c in template.iter() {
-            let count = if let Some(n) = counts.get(c) {
-                *n + 1
-            } else {
-                1
-            };
-            counts.insert(*c, count);
-        }
-        if slice - 1 != 0 {
-            *counts.get_mut(&template[0]).unwrap() -= 1;
+        pair_counts = new_pair_counts;
+        if i == 9 {
+            let p1 = calc_answer(&pair_counts, last);
+            println!("Day 14 Part 1: {}", p1);
         }
     }
-    
-    let p2 = counts.values().max().unwrap() - counts.values().min().unwrap();
+    let p2 = calc_answer(&pair_counts, last);
     println!("Day 14 Part 2: {}", p2);
 }
 
-#[inline(always)]
-fn do_insertions(s: &[u8], rules: &HashMap<(u8, u8), u8>) -> Vec<u8> {
-    let mut next = Vec::new();
-    for i in 0..s.len() - 1 {
-        next.push(s[i]);
-        if let Some(c) = rules.get(&(s[i], s[i + 1])) {
-            next.push(*c);
-        }
+fn calc_answer(pair_counts: &HashMap<(u8, u8), usize>, last: u8) -> usize {
+    let mut counts: HashMap<u8, usize> = HashMap::new();
+    for (pair, &count) in pair_counts {
+        *counts.entry(pair.0).or_default() += count;
     }
-    next.push(s[s.len() - 1]);
-    next
+    *counts.entry(last).or_default() += 1;
+    counts.values().max().unwrap() - counts.values().min().unwrap()
 }
